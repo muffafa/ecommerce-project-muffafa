be17db0c0eaec7752c8c00d1b1ee82a2105c7d64
muffafa <mm.savar@gmail.com>
2024-05-11 17:05:26 +0300

    feat: react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import './App.css'
 import Populer from './Components/Populer/Populer'
 import Teklifler from './Components/Teklifler/Teklifler'
 import Newsletter from './Components/Newsletter/Newsletter'
 import YeniUrunler from './Components/YeniUrunler/YeniUrunler'
-import Footer from './Components/Footer/Footer'
-import LoginSignup from './Pages/LoginSignup'
-import SingleProduct from './Components/SingleProduct/SingleProduct'
 
 function App() {
 
   return (
     <>
-    <Navbar></Navbar>
     <Hero></Hero>
     <Populer></Populer>
     <Teklifler></Teklifler>
     <YeniUrunler></YeniUrunler>
     <Newsletter></Newsletter>
-    <Footer></Footer>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import MarketContextProvider from './Context/MarketContext.jsx'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import {Outlet,  RouterProvider, createBrowserRouter } from "react-router-dom"
+import LoginSignup from "./Pages/LoginSignup.jsx";
+import SingleProduct from './Components/SingleProduct/SingleProduct'
+import Market from './Pages/Market.jsx'
+import Cart from './Pages/Cart'
+
+const Layout = () => {
+  return(
+    <div>
+      <Navbar></Navbar>
+      <Outlet/>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([{
+  path:'/',
+  element: <Layout />,
+  children:[
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/login",
+      element: <LoginSignup />,
+    },
+    {
+      path: "/product",
+      element: <SingleProduct />
+    },
+    {
+      path: "/market",
+      element: <Market/>,
+    },
+    {
+      path: "/cart",
+      element: <Cart/>,
+    }
+  ], 
+
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <MarketContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <RouterProvider router={router} />
   </MarketContextProvider>
 
 )
